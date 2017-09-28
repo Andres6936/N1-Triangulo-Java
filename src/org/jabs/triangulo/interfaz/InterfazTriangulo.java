7481bedaff3bc8ef4d2b28996abdf1765b89f676
Joan Andrés <andres6936@live.com>
2017-09-28 16:02:35 -0500

    0.f.a

--- src/org/jabs/triangulo/interfaz/InterfazTriangulo.java
+++ src/org/jabs/triangulo/interfaz/InterfazTriangulo.java
@@ -121,4 +121,121 @@ public class InterfazTriangulo extends JFrame
     // Métodos
     // -----------------------------------------------------------------
 
+    /**
+     * Muestra el diálogo para cambiar los puntos del triángulo
+     */
+    public void cambiarPuntos( )
+    {
+        dialogoPuntos = new DialogoPuntos( this );
+        dialogoPuntos.setVisible( true );
+        setEnabled( false );
+    }
+
+    /**
+     * Cambia el color del fondo
+     *
+     */
+    public void cambiarColorFondo( )
+    {
+        Color colorActual = new Color( triangulo.darColorRelleno( ).darRojo( ), triangulo.darColorRelleno( ).darVerde( ), triangulo.darColorRelleno( ).darAzul( ) );
+        Color nuevoColor = JColorChooser.showDialog( this, "Color Fondo", colorActual );
+        if( nuevoColor != null )
+        {
+            //Cambia los valores
+            triangulo.darColorRelleno( ).cambiarRojo( nuevoColor.getRed( ) );
+            triangulo.darColorRelleno( ).cambiarVerde( nuevoColor.getGreen( ) );
+            triangulo.darColorRelleno( ).cambiarAzul( nuevoColor.getBlue( ) );
+        }
+        repintar( );
+    }
+    /**
+     * Cambia el color de las líneas
+     *
+     */
+    public void cambiarColorLineas( )
+    {
+        Color colorActual = new Color( triangulo.darColorLineas( ).darRojo( ), triangulo.darColorLineas( ).darVerde( ), triangulo.darColorLineas( ).darAzul( ) );
+        Color nuevoColor = JColorChooser.showDialog( this, "Color Líneas", colorActual );
+        if( nuevoColor != null )
+        {
+            //Cambia los valores
+            triangulo.darColorLineas( ).cambiarRojo( nuevoColor.getRed( ) );
+            triangulo.darColorLineas( ).cambiarVerde( nuevoColor.getGreen( ) );
+            triangulo.darColorLineas( ).cambiarAzul( nuevoColor.getBlue( ) );
+        }
+        repintar( );
+    }
+
+    /**
+     * Repinta el triángulo y los valores
+     *
+     */
+    public void repintar( )
+    {
+        panelTriangulo.repaint( );
+        panelBotones.cambiarInformacion( triangulo.darPerimetro( ), triangulo.darArea( ), triangulo.darAltura( ) );
+    }
+
+    /**
+     * Devuelve el triángulo actual
+     * @return Triángulo
+     */
+    public Triangulo darTriangulo( )
+    {
+        return triangulo;
+    }
+
+    /**
+     * Verifica si 3 puntos son colineales
+     * @param p1x Valor X punto 1
+     * @param p1y Valor Y punto 1
+     * @param p2x Valor X punto 2
+     * @param p2y Valor Y punto 2
+     * @param p3x Valor X punto 3
+     * @param p3y Valor Y punto 3
+     * @return verdadero si los tres puntos dados son colineales
+     */
+    public boolean colineales( double p1x, double p1y, double p2x, double p2y, double p3x, double p3y )
+    {
+        //Verifica usando un Line2D
+        Line2D linea = new Line2D.Double( p1x, p1y, p2x, p2y );
+        return ( linea.ptLineDist( p3x, p3y ) == 0 );
+
+    }
+
+    //-----------------------------------------------------------------
+    // Puntos de Extensión
+    //-----------------------------------------------------------------
+
+    /**
+     * Ejecuta el punto de extensión 1
+     */
+    public void reqFuncOpcion1( )
+    {
+        String resultado = triangulo.metodo1( );
+        JOptionPane.showMessageDialog( this, resultado, "Respuesta", JOptionPane.INFORMATION_MESSAGE );
+    }
+
+    /**
+     * Ejecuta el punto de extensión 2
+     */
+    public void reqFuncOpcion2( )
+    {
+        String resultado = triangulo.metodo2( );
+        JOptionPane.showMessageDialog( this, resultado, "Respuesta", JOptionPane.INFORMATION_MESSAGE );
+    }
+
+    // -----------------------------------------------------------------
+    // Main
+    // -----------------------------------------------------------------
+
+    /**
+     * Inicia la aplicación
+     * @param args Parámetros no utilizados
+     */
+    public static void main( String[] args )
+    {
+        InterfazTriangulo interfaz = new InterfazTriangulo( );
+        interfaz.setVisible( true );
+    }
 }
