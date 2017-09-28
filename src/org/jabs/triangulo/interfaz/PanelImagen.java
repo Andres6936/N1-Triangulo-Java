929e2dcdf60db7dff58446b832700d0cbacbaafb
Joan Andrés <andres6936@live.com>
2017-09-28 16:09:45 -0500

    0.h

--- src/org/jabs/triangulo/interfaz/PanelImagen.java
+++ src/org/jabs/triangulo/interfaz/PanelImagen.java
@@ -1,9 +1,17 @@
 package org.jabs.triangulo.interfaz;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
 /**
  * Creado por Familia Buriticá en 9/28/2017.
  */
-public class PanelImagen
+public class PanelImagen extends JPanel
 {
 
     // -----------------------------------------------------------------
@@ -15,14 +23,36 @@ public class PanelImagen
     // Atributos
     // -----------------------------------------------------------------
 
+    /**
+     * Imagen del título
+     */
+    private JLabel imagen;
 
     // -----------------------------------------------------------------
     // Constructor
     // -----------------------------------------------------------------
 
-    public PanelImagen()
+    /**
+     * Constructor sin parámetros
+     */
+    public PanelImagen( )
     {
-
+        FlowLayout layout = new FlowLayout( );
+        layout.setHgap( 0 );
+        layout.setVgap( 0 );
+        setLayout( layout );
+        //
+        //Carga la imagen
+        ImageIcon icono = new ImageIcon( "data/titulo.png" );
+        //
+        //La agrega a la etiqueta
+        imagen = new JLabel( "" );
+        imagen.setIcon( icono );
+        add( imagen );
+        //
+        //Color de fondo blanco
+        setBackground( Color.WHITE );
+        setBorder( new LineBorder( Color.GRAY ) );
     }
 
     // -----------------------------------------------------------------
